4b1b4527d5939820e7067a34365fc2bb59092ca3
margintradings777-sudo <margintradings777@gmail.com>
2026-01-21 04:57:48 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"status": "Backend running"})
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("auth/", include("tables.urls")),
-    path("auth/", include("apis.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
