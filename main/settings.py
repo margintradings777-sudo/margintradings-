ee18beb86b1ac32878726462de36b8d7073fd9a7
margintradings777-sudo <margintradings777@gmail.com>
2026-01-21 01:43:00 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -98,12 +98,10 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # =========================
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR}/db.sqlite3"),
-        conn_max_age=600,
-        # Set to True in production to ensure SSL encryption for Postgres
-        ssl_require=not DEBUG, 
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 # =========================
