f6c9807c43e0a6e633bb993408f9e1c157f0daed
margintradings777-sudo <margintradings777@gmail.com>
2026-01-21 05:27:23 +0530

    Update urls.py

--- tables/urls.py
+++ tables/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login_view, register_view
+from .views import login_view, register_view, profile_view
 
 urlpatterns = [
     path("login/", login_view),
     path("register/", register_view),
+    path("profile/<int:user_id>/", profile_view),
 ]
