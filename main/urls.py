7da3b7bcb2b21cce946e12cdc203f9ed45c251b2
margintradings777-sudo <margintradings777@gmail.com>
2026-01-21 05:17:46 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
-
-def home(request):
-    return JsonResponse({"status": "Backend running"})
 
 urlpatterns = [
-    path("", home),
     path("admin/", admin.site.urls),
     path("auth/", include("tables.urls")),
 ]
