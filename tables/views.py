443457a5f76cdfd354494052e4386a60eae02a68
margintradings777-sudo <margintradings777@gmail.com>
2026-01-27 13:59:53 +0530

    Add password hashing functions to views.py

--- tables/views.py
+++ tables/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.contrib.auth.hashers import make_password, check_password
+from .models import UserDetail
 
 @csrf_exempt
 def withdrawal_view(request):
