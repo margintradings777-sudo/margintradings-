6867f0a623ff647b790d26eeee59caf8d22bbc8f
margintradings777-sudo <margintradings777@gmail.com>
2026-01-27 11:29:36 +0530

    Add withdrawal_view to URL patterns

--- tables/urls.py
+++ tables/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login_view, register_view, profile_view
+from .views import login_view, register_view, profile_view, withdrawal_view
 
 urlpatterns = [
     path("login/", login_view),
