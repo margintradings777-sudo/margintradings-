ff2f21097f8c57c64ea4752b4e777df993698259
margintradings777-sudo <margintradings777@gmail.com>
2026-02-02 16:47:31 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", lambda request: HttpResponse("Backend Running Successfully 🚀")),
     path("admin/", admin.site.urls),
-    path("auth/", include("tables.urls")),
+
+    # APIs (login, register, withdrawal, deposit etc)
+    path("apis/v1/", include("apis.urls")),
+
+    # Tables app (profile view etc)
+    path("", include("tables.urls")),
 ]
+
+# Media files (PAN image, bank document etc)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
