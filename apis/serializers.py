fcad9b39c8928dc985bff805f3bdd19c8a51eee0
margintradings777-sudo <margintradings777@gmail.com>
2026-02-02 15:18:44 +0530

    Update serializers.py

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,29 +1,26 @@
 from rest_framework import serializers
-from tables.models import UserDetail
+from tables.models import UserDetail, Deposit, withdrawal, DepositAccountDetails
+
 
 class UserDetailSerializer(serializers.ModelSerializer):
-    Pan = serializers.CharField(source="PAN_No", required=False, allow_blank=True)
-    IFSC_code = serializers.CharField(source="IFSC_Code", required=False, allow_blank=True)
-
     class Meta:
         model = UserDetail
         fields = "__all__"
-        # This prevents the password from being leaked in GET requests
-        extra_kwargs = {
-            'Password': {'write_only': True}
-        }
+
 
 class DepositSerializer(serializers.ModelSerializer):
     class Meta:
         model = Deposit
-        fields = '__all__'
+        fields = "__all__"
+
 
 class WithdrawalSerializer(serializers.ModelSerializer):
     class Meta:
         model = withdrawal
-        fields = ['id', 'Name', 'Amount', 'Status', 'user']
+        fields = "__all__"
+
 
 class DepositAccountDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = DepositAccountDetails
-        fields = '__all__'
+        fields = "__all__"
