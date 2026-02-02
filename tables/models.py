4670091350c1b3286449545861f4d1f15865ae2a
margintradings777-sudo <margintradings777@gmail.com>
2026-02-02 12:54:32 +0530

    Update models.py

--- tables/models.py
+++ tables/models.py
@@ -2,15 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
-class CustomerProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="customer_profile")
-    phone = models.CharField(max_length=15, blank=True, null=True)
-    pan_no = models.CharField(max_length=10, blank=True, null=True)
-    pan_image = models.ImageField(upload_to="kyc/pan/", blank=True, null=True)
-    account_number = models.CharField(max_length=30, blank=True, null=True)
-    ifsc_code = models.CharField(max_length=20, blank=True, null=True)
-    bank_document = models.FileField(upload_to="kyc/bank_docs/", blank=True, null=True)
-    account_balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+class UserDetail(models.Model):
+    Name = models.CharField(max_length=100)
+    Email = models.EmailField(unique=True)
+    Password = models.CharField(max_length=255)
+    Account_Balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+
+    Phone = models.CharField(max_length=15, blank=True, null=True)
+    PAN_No = models.CharField(max_length=10, blank=True, null=True)
+    PAN_Image = models.ImageField(upload_to="kyc/pan/", blank=True, null=True)
+
+    Account_No = models.CharField(max_length=30, blank=True, null=True)
+    IFSC_Code = models.CharField(max_length=20, blank=True, null=True)
+
+    Bank_Document = models.FileField(upload_to="kyc/bank_docs/", blank=True, null=True)
+
     def __str__(self):
         return self.user.username
 
@@ -26,7 +32,7 @@ def __str__(self):
         return self.BankName
 
 class withdrawal(models.Model):
-    user = models.ForeignKey(CustomerProfile, on_delete=models.CASCADE, related_name='withdrawals')
+    user = models.ForeignKey(UserDetail, on_delete=models.CASCADE, related_name='withdrawals')
     Name=models.CharField(max_length=200,blank=True,null=True)
     Amount=models.CharField(max_length=200,blank=True,null=True)
     Status=models.CharField(max_length=200,blank=True,null=True)
